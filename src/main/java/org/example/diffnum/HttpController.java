6619a088a009eae70c6e40c52b86e92f56e8ca1e
kh <hattorikonan@MacBook-Air-5.local>
2024-05-09 22:56:15 +0900

    使い方ページ、フッター、ヘッダーを一新しました

--- src/main/java/org/example/diffnum/HttpController.java
+++ src/main/java/org/example/diffnum/HttpController.java
@@ -10,9 +10,17 @@ public class HttpController {
     public String showHomePage(Model model) {
         return "html/home";  // templates/html/home.htmlへのビュー名を返す
     }
-
+    @RequestMapping("/home/privacy-policy")
+    public String showPrivacyPolicy(Model model) {
+        return "html/privacy-policy";  // templates/html/home.htmlへのビュ
+    }
     @RequestMapping("/home/manualCal")
     public String showManualCalPage(Model model) {
         return "html/manual";  // templates/html/manual.htmlへのビュー名を返す
+    }
+
+    @RequestMapping("/home/manualCal/how-to")
+    public String showManualCalHowTo(Model model) {
+        return "html/how-to";  // templates/html/manual.htmlへのビュー名を返す
     }
 }
