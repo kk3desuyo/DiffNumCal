2f5da1b97860a950ec275f3cde590c13cae77832
kh <hattorikonan@MacBook-Air-5.local>
2024-05-14 17:27:12 +0900

    ぶどうシミュレーターの実装とファイル選択ボタンの位置変更を実装しました。

--- src/main/resources/static/js/manual.js
+++ src/main/resources/static/js/manual.js
@@ -22,7 +22,7 @@ function loadImage(obj) {
                 drawingCanvas.width = graph.clientWidth - 20; // paddingを除外した幅
                 drawingCanvas.height = window.innerHeight * 0.6;
 
-                graph.style.height = (previewCanvas.height+ 60) + 'px';
+                graph.style.height = (previewCanvas.height+ 90) + 'px';
                 console.log('グラフサイズ: ' + graph.style.height);
 
                 // アスペクト比を無視して、canvas全体に画像をフィットさせる
@@ -120,7 +120,7 @@ function diffNumCal() {
     diffNumSheets = finalY < minMemoryY ? diffNumSheets : -diffNumSheets;
     console.log('差枚数 :'+diffNumSheets);
 
-    document.getElementById('result').innerText = diffNumSheets + '枚';
+    document.getElementById('diff-num').innerText = diffNumSheets;
 }
 
 //スライダーの初期化
@@ -198,4 +198,133 @@ function drawLine(){
 
 }
 
-//スライダー
+//機種の確率情報を管理するクラス
+class Model{
+    //I'm
+    iam_cherry_payout = 2
+    iam_grape_payout = 8
+    iam_replay_payout = 3
+    iam_big_payout = 252
+    iam_reg_payout = 96
+    iam_big = [273.1,269.7,269.7,259.0,259.0,255.0]
+    iam_reg = [439.8,399.6,331.0,315.1,255.0,255.0]
+    iam_big_ch = [1129.93,1129.93,1129.93,1129.93,1092.27,1092.27,1092.27]
+    iam_reg_ch = [1394.38,1236.53,1092.27,1057.03,851.12,851.12]
+    iam_grape =[6.02,6.02,6.02,6.02,6.02,5.78]
+    iam_cherry =32.29
+    iam_clown　=1092.27
+    iam_bell　=1092.27
+    iam_replay = 7.3
+
+    constructor(model){
+        //I'mジャグラー
+        if(model === "iam"){
+            this.cherry_payout = this.iam_cherry_payout;
+            this.replay_payout = this.iam_replay_payout
+            this.grape_payout  = this.iam_grape_payout
+            this.big_payout = this.iam_big_payout;
+            this.reg_payout = this.iam_reg_payout;
+            this.big = this.iam_big;
+            this.reg = this.iam_reg
+            this.big_ch = this.iam_big_ch
+            this.reg_ch = this.iam_reg_ch
+            this.grape = this.iam_grape;
+            this.clown = this.iam_clown;
+            this.bell = this.iam_bell;
+            this.cherry = this.iam_cherry;
+            this.replay = this.iam_replay;
+        }
+
+    }
+
+    static calculateReturns(model,num_of_revolutions) {
+        //チェリー
+        const cherry_hits = num_of_revolutions / model.cherry ;
+        const replay_hits = num_of_revolutions / model.replay;
+        const bell_hits = num_of_revolutions/model.bell;
+        const clown_hits = num_of_revolutions/model.clown;
+        console.log(cherry_hits,"   ",replay_hits,"   ",bell_hits,"   ",clown_hits);
+        return {
+            cherry: cherry_hits,
+            replay: replay_hits,
+            bell:bell_hits,
+            clown:clown_hits
+        };
+
+    }
+     static grape_simulate(){
+        //入力値の取得
+        var big_count = sanitize(document.getElementById('big-count').value)
+        var reg_count =sanitize(document.getElementById('reg-count').value)
+        var num_of_revolutions = sanitize(document.getElementById('revolutions').value)
+        var model_input = document.getElementById('model').value
+        var diff_num = parseInt(document.getElementById('diff-num').innerText)
+        //  var big_count = 24
+        //  var reg_count =31
+        //  var num_of_revolutions = 6671
+        //  var model_input = 'iam'
+        //  var diff_num = 1180
+
+        console.log('big',big_count)
+        console.log('reg',reg_count)
+        console.log('revo',num_of_revolutions)
+        console.log('iam',model_input)
+        console.log('diff',diff_num)
+
+         //機種の割り当て
+         var model = new Model(model_input);
+        console.log('情報',model.replay)
+        console.log(big_count)
+        console.log(reg_count)
+        console.log(num_of_revolutions)
+        //使用枚数を計算(リプレイ等は無視) 3枚掛け * 回転数
+        var used_num = 3 * num_of_revolutions;
+        console.log(used_num)
+        //ビッグ,regの払い出し枚数 - (1ベットで揃える時の1マイ)
+        var big_return = model.big_payout * big_count - big_count;
+        var reg_return = model.reg_payout * reg_count - reg_count;
+        console.log('big'+big_return)
+        console.log('reg'+reg_return)
+
+        var hits_infos = this.calculateReturns(model,num_of_revolutions);
+        console.log(hits_infos);
+        //チェリーの払い出しを計算
+        var cherry_return = hits_infos.cherry * model.cherry_payout;
+
+        //リプレイの払い戻し
+        var replay_return = hits_infos.replay * 3;
+
+        //ベル
+        var bell_return = hits_infos.bell * 14;
+        //ピエロ
+        var clown_return = hits_infos.clown*14;
+
+        var total_return = (big_return + reg_return + cherry_return+replay_return + bell_return + clown_return)
+        var grape_return =  used_num + diff_num - total_return;
+        console.log(used_num)
+         console.log(diff_num)
+         console.log(total_return)
+         // 各払い戻し情報をコンソールに出力
+         console.log('Cherry Return: ' + cherry_return);
+         console.log('Replay Return: ' + replay_return);
+         console.log('Bell Return: ' + bell_return);
+         console.log('Clown Return: ' + clown_return);
+         console.log('Total Return: ' + total_return);
+         console.log('used',used_num)
+         console.log('diff',diff_num)
+         console.log('total',total_return)
+         console.log(grape_return)
+        console.log('ぶどうの枚数ue')
+        //（払い戻し(ぶどう以外)+ぶどう) - 投入枚数　＝差枚数
+        var grape_count = grape_return/8;
+        console.log('ぶどうの個数',grape_count)
+        //分数の変換
+        var grape_probability = grape_count/num_of_revolutions;
+        console.log(grape_probability)
+        grape_probability = 100/(grape_probability*100);
+        console.log('武道確率',grape_probability)
+         //武道確率表示部分の変更
+         document.getElementById('grape-probability').innerText = Math.round(grape_probability * 100) / 100;
+    }
+}
+
